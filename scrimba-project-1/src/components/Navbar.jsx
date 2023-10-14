a6578c5c3713b2c47eb4622557eb425689f7fbc8
Christopher Høe <christopher.hoee@gmail.com>
2023-10-14 21:16:13 +0200

    feat: completed challenge 2

--- scrimba-project-1/src/components/Navbar.jsx
+++ scrimba-project-1/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
+import logo from "../assets/reactjs-icon.svg";
+
 const Navbar = () => {
   return (
     <header>
-      <nav>
-        <h3>ReactFacts</h3>
-        <h4>React Course - Project 1</h4>
+      <nav className="nav">
+        <img src={logo} className="nav--icon" />
+        <h3 className="nav--logo-text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
       </nav>
     </header>
   );
