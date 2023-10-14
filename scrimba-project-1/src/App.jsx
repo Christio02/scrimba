b19440d1d5059fee1bad8ef8af095767b808cc28
Christopher Høe <christopher.hoee@gmail.com>
2023-10-14 20:33:24 +0200

    initial commit

---
+++ scrimba-project-1/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+
+function App() {
+  return (
+    <div className="main-container">
+      <Navbar />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ scrimba-project-1/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
