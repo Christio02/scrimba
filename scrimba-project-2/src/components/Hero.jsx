8804ba7094c7cff92488e00cb8d755a2c38d9840
Christopher Høe <christopher.hoee@gmail.com>
2023-11-05 19:31:53 +0100

    feat: finished navbar and hero component

---
+++ scrimba-project-2/src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import HeroImage from "../../public/hero-images.svg";
+
+const Hero = () => {
+  return (
+    <section className="hero-container">
+      <div className="image-container">
+        <img src={HeroImage}></img>
+      </div>
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by <br></br>one-of-a-kind
+        hosts—all without leaving home.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ scrimba-project-2/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Logo from "../../public/airbnb1.svg";
+
+const Navbar = () => {
+  return (
+    <header className="header-nav">
+      <nav className="nav">
+        <img src={Logo} className="airbnd-logo1"></img>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
