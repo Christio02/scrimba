1add113089f8014f0803a912c678969d8e2057ae
Christio02 <112630243+Christio02@users.noreply.github.com>
2023-12-21 00:22:30 +0100

    feat: finished project 2 - Refactor card display and add badges - initialized travel journal solo project

--- scrimba-project-2/src/App.jsx
+++ scrimba-project-2/src/App.jsx
@@ -4,34 +4,13 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Data from "./data";
 
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
-
 function App() {
-  const cardElements = Data.map((card) => (
-    <Card
-      key={card.id}
-      img={card.coverImg}
-      rating={card.stats.rating}
-      reviewCount={card.stats.reviewCount}
-      country={card.location}
-      price={card.price}
-    />
-  ));
+  const cardElements = Data.map((card) => <Card key={card.id} {...card} />);
   return (
     <div>
       <Navbar />
-      {/* <Hero></Hero> */}
-      <div>{cardElements}</div>
+      <Hero />
+      <div className="cards-container">{cardElements}</div>
     </div>
   );
 }
--- scrimba-project-2/src/components/Card.jsx
+++ scrimba-project-2/src/components/Card.jsx
@@ -2,30 +2,38 @@ import PropTypes from "prop-types";
 import Star from "/images/Star1.png";
 
 Card.propTypes = {
-  img: PropTypes.string.isRequired,
-  rating: PropTypes.string.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
   reviewCount: PropTypes.number.isRequired,
-  country: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+  openSpots: PropTypes.number.isRequired,
+  stats: PropTypes.shape({
+    rating: PropTypes.number.isRequired,
+    reviewCount: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <div className="card-container">
+      {badgeText ? <div className="card-badge">{badgeText}</div> : null}
       <div className="card-image-container">
-        <span>
-          <p>SOLD OUT</p>
-        </span>
-
-        <img src={props.img}></img>
+        <img src={props.coverImg}></img>
       </div>
 
       <div className="rating-container">
         <img src={Star} className="star"></img>
-        <span>{props.rating}</span>
-        <span className="gray"> ({props.reviewCount}) * </span>
-        <span className="gray">{props.country}</span>
+        <span>{props.stats.rating}</span>
+        <span className="gray"> ({props.stats.reviewCount}) * </span>
+        <span className="gray">{props.location}</span>
       </div>
       <p>{props.title}</p>
       <p>
